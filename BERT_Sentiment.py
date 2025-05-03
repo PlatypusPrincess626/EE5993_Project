025805c4657eeb8d39ccd105ee59d840daa5f026
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2025-05-02 22:13:19 -0500

    More Batches

--- BERT_Sentiment.py
+++ BERT_Sentiment.py
@@ -20,6 +20,7 @@ print(f"Using device: {device}")
 
 # Load dataset
 df = pd.read_csv('emotion_dataset_raw.csv')
+df = df[~df["Label"].isin(["disgust", "shame"])]
 
 # Encode labels
 label_encoder = LabelEncoder()
