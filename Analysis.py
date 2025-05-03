800700009d9bc6364e3824be43362cc42922f15e
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2025-05-03 18:30:57 -0500

    More Batches

--- Analysis.py
+++ Analysis.py
@@ -2,10 +2,12 @@ import pandas as pd
 
 # Step 1: Read your CSV file
 df = pd.read_csv('BERT_mental_dataset_predictions.csv', sep='|')
-print(df.head())
+df_gpt2 = pd.read_csv('GPT2_mental_dataset_predictions.csv', sep='|')
 
 # Step 2: Generate a cross-tabulation (contingency table)
 result = pd.crosstab(df['prediction'], df['label'])
+result_gpt2 = pd.crosstab(df_gpt2['prediction'], df_gpt2['label'])
 
 # Step 3: Display the result
 print(result)
+print(result_gpt2)
