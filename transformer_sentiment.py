e0a7e7b2bf46a0823b55aa87c91909c7b14379fd
PlatypusPrincess626 <masoncoleconkel@outlook.com>
2025-05-04 16:40:27 -0500

    More Batches

--- transformer_sentiment.py
+++ transformer_sentiment.py
@@ -172,7 +172,7 @@ decoded_preds = label_encoder.inverse_transform(predictions)
 with open('transformer_mental_dataset_predictions.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile, delimiter='|', quoting=csv.QUOTE_MINIMAL)
     writer.writerow(['prediction', 'label', 'text'])
-    for pred, text in zip(decoded_preds, new_df['label'].tolist(), new_df['statement'].tolist()):
+    for pred, text in zip(decoded_preds, new_df['status'].tolist(), new_df['statement'].tolist()):
         writer.writerow([pred, text])
 
 print("Prediction file saved: transformer_mental_dataset_predictions.csv")
